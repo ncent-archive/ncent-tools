f040eb88b4272d1e00cb8f371080d38faeb96b81
Dustin Pearson <dustinparkerpearson@gmail.com>
2018-09-07 15:16:43 -0700

    Solve throttling issue for AWS SES

--- emailScript/src/send.rb
+++ emailScript/src/send.rb
@@ -6,9 +6,8 @@ require_relative '../utils/utils'
 
 $client = Aws::SES::Client.new(region: 'us-west-2')
 
-def construct_bulk_email()
-  recipient_data = csv_to_array
-  bulk_template = {
+def generate_bulk_template
+  {
     source: $config[:from],
     template: $config[:template_name],
     destinations: [],
@@ -17,42 +16,62 @@ def construct_bulk_email()
       last_name: $config[:default_last_name]
     }.to_json
   }
-  recipient_data.each do |recipient|
-    # TODO put limit on recipient iterations based on throttling
-    destination = {
-      destination: {
-        to_addresses: [ recipient[:email] ],
-        cc_addresses: $config[:cc_addresses],
-        bcc_addresses: $config[:bcc_addresses],
-      },
-      replacement_template_data: {
-        first_name: recipient[:first_name],
-        last_name: recipient[:last_name]
-      }.to_json
-    }
-    bulk_template[:destinations].push(destination)
-  end
-
-  bulk_template
 end
 
-def send_bulk_email()
-  create_template
-  bulk_template = construct_bulk_email
-  resp = $client.send_bulk_templated_email(bulk_template)
+def construct_destination(recipient)
+  {
+    destination: {
+      to_addresses: [ recipient[:email] ],
+      cc_addresses: $config[:cc_addresses],
+      bcc_addresses: $config[:bcc_addresses],
+    },
+    replacement_template_data: {
+      first_name: recipient[:first_name],
+      last_name: recipient[:last_name]
+    }.to_json
+  }
+end
+
+def construct_bulk_templates()
+  recipients = csv_to_array
+  bulk_templates = []
+  bulk_template = nil # Needed scope. Ruby doesn't allow dec without init
+  recipients.each_with_index do |recipient, i|
+    if (i % $config[:throttle_rate] == 0)
+      bulk_template = generate_bulk_template
+      bulk_templates << bulk_template
+    end
+    bulk_template[:destinations] << construct_destination(recipient)
+  end
+  bulk_templates
+end
+
+def send_bulk_email_chunk(chunk)
+  resp = $client.send_bulk_templated_email(chunk)
   status = resp.status[0].status
   error = resp.status[0].error
   if (error != nil)
-    if (status == "AccountThrottled")
-
-    else
-      raise "Unexpected error that is not related to account throttling"
-    end
+    raise "Error unrelated to account throttle" if status != "AccountThrottled"
+    return false
   else
-    puts "Successfully sent emails"
-    puts "Deleting template...#{$config[:template_name]}"
-    delete_email_template($config[:template_name])
+    puts "Successfully sent #{chunk[:destinations].length} emails"
+    return true
   end
 end
 
-send_bulk_email()
+def send_bulk_emails()
+  create_template
+  bulk_templates = construct_bulk_templates
+  bulk_templates.each_with_index do |bulk_template|
+    success = false
+    until success
+      success = send_bulk_email_chunk(bulk_template)
+      sleep(1.1) unless success
+    end
+    sleep(1.1)
+  end
+  puts "Deleting template named '#{$config[:template_name]}'"
+  delete_email_template($config[:template_name])
+end
+
+send_bulk_emails()
